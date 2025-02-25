edf3cbd226636b96442f2b12e36fd15f36b6217d
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-25 11:38:49 +0700

    `add` : content about page

--- resources/views/user/profile/about/index.blade.php
+++ resources/views/user/profile/about/index.blade.php
@@ -1,15 +1,27 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Tentang Sentra Halal") }}
+                <div class="p-6 flex items-center justify-between">
+                    <div class="w-1/2">
+                        <h2 class="text-lg font-bold mb-4">Tentang Sentra Halal Universitas Muhammadiyah Purwokerto</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Gencar dan bebasnya perdagangan internasional menjadikan Indonesia mudah dimasuki produk impor yang rentan mengandung bahan tidak halal. Oktober 2014 telah disahkan UU Republik Indonesia No 33 tahun 2014 tentang Jaminan Produk Halal (UU-JPH). Produk makanan, minuman, kosmetik, dan obat-obatan yang masuk, beredar, dan diperdagangkan di wilayah Indonesia wajib bersertifikat halal. Sertifikat halal meliputi pemeriksaan bahan baku, bahan tambahan, teknik pemrosesan, pengepakan, logistik, penyimpanan, kemasan dan penyajian.
+                        </p>
+                        <h3 class="text-md font-semibold mt-4">Proses Pendirian Sentra Halal di Universitas Muhammadiyah Purwokerto:</h3>
+                        <ul class="list-disc list-inside text-sm text-gray-700 text-justify">
+                            <li>20 Maret 2016 - Tim Perumus</li>
+                            <li>1 Agustus 2018 - SK tentang Pengurus Sentra Halal</li>
+                            <li>12 Desember 2019 - Launching Sentra Halal</li>
+                            <li>23 Maret 2020 - SK tentang Pengangkatan Ketua Pusat Studi di UMP</li>
+                        </ul>
+                    </div>
+
+                    <div class="w-1/2 flex justify-end">
+                        <div class="w-100 h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/image-about.png') }}" alt="Logo">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
