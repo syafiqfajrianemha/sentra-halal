b8b233454c2a46bddb3b269be0ea853a12493843
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-25 13:39:06 +0700

    `add` : content vision and mission

--- resources/views/user/profile/visionmission/index.blade.php
+++ resources/views/user/profile/visionmission/index.blade.php
@@ -1,15 +1,56 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Visi dan Misi") }}
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-80 h-80 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/image-visi.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">Visi</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Menjadi lembaga pendidikan, penelitian, pengabdian masyarakat dalam pengembangan ilmu pengetahuan dan teknologi dalam sistem jaminan produk halal di Indonesia.
+                        </p>
+                    </div>
+                </div>
+
+                <div class="mt-12 p-6">
+                    <h2 class="text-xl font-bold text-center mb-6">Misi</h2>
+                    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+                        <div class="bg-yellow-400 p-4 rounded-lg shadow-md">
+                            <h3 class="font-bold">Pengetahuan dan Teknologi</h3>
+                            <ul class="list-disc list-inside text-sm text-gray-700">
+                                <li>Membangun ilmu pengetahuan dan teknologi terkait kehalalan suatu produk.</li>
+                                <li>Melaksanakan pengkajian saintifik dan pengembangan produk dan teknologi untuk produk halal.</li>
+                                <li>Melaksanakan kajian syariah dan publikasi terkait standar jaminan produk halal.</li>
+                            </ul>
+                        </div>
+                        <div class="bg-red-500 text-white p-4 rounded-lg shadow-md">
+                            <h3 class="font-bold">Kerjasama Dalam dan Luar Negeri</h3>
+                            <ul class="list-disc list-inside text-sm">
+                                <li>Membangun jaringan kerjasama institusi dalam dan luar negeri terkait kehalalan.</li>
+                            </ul>
+                        </div>
+                        <div class="bg-green-400 p-4 rounded-lg shadow-md">
+                            <h3 class="font-bold">Sosial dan Masyarakat</h3>
+                            <ul class="list-disc list-inside text-sm text-gray-700">
+                                <li>Melakukan pengabdian kepada masyarakat & pelatihan tentang halal.</li>
+                                <li>Melaksanakan pembinaan UMKM agar mampu menilai standar jaminan produk halal.</li>
+                                <li>Membangun masyarakat yang peduli dengan kehidupan halal.</li>
+                            </ul>
+                        </div>
+                        <div class="bg-blue-500 text-white p-4 rounded-lg shadow-md">
+                            <h3 class="font-bold">Pelayanan dan Pengembangan</h3>
+                            <ul class="list-disc list-inside text-sm">
+                                <li>Memberikan pelayanan pemeriksaan halal.</li>
+                                <li>Memberikan pelayanan konsultasi penerapan standar jaminan produk halal.</li>
+                                <li>Membangun sistem pendidikan dan pengembangan SDM untuk keperluan Sistem Jaminan Produk Halal.</li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
