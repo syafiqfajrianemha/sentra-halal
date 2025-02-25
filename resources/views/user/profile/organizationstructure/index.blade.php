d89b6895b57c11017fcf5ac81724a765ed4fbdbe
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-25 13:55:14 +0700

    `add` : content structure organization

--- resources/views/user/profile/organizationstructure/index.blade.php
+++ resources/views/user/profile/organizationstructure/index.blade.php
@@ -1,15 +1,72 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Struktur Organisasi") }}
+                <div class="p-6">
+                    <h2 class="text-lg font-bold mb-4 text-center">Struktur Organisasi Sentra Halal Universitas Muhammadiyah Purwokerto</h2>
+                    <table class="min-w-full border-collapse border border-gray-300">
+                        <thead>
+                            <tr class="bg-blue-500 text-white">
+                                <th class="border border-gray-300 p-2">Jabatan</th>
+                                <th class="border border-gray-300 p-2">Nama</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold">Ketua Sentra Halal</td>
+                                <td class="border border-gray-300 p-2">Dr. apt. Diniatik, M.Sc.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold">Sekretaris</td>
+                                <td class="border border-gray-300 p-2">Dr. apt. Ika Yuni Astuti, S.Si., M.Si.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold">Bendahara</td>
+                                <td class="border border-gray-300 p-2">Apt. Fitriyani, M.S.Farm.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold" rowspan="3">Laboratorium Riset Halal</td>
+                                <td class="border border-gray-300 p-2">Dr. apt. Wiranti SR, S.F., M.Sc.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Dr. Asmiyenti DD, S.Si., M.Sc.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Dr. apt. Prilswati U, S.Si., M.Sc.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold" rowspan="3">Humas Publikasi dan Bisnis Halal</td>
+                                <td class="border border-gray-300 p-2">Arif Mulyanto, S.Si., M.Si.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Istianah, Lc. M.Hum.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Apt. Aditya Singgih R, S.Farm.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold" rowspan="2">Lembaga Pelatihan Halal</td>
+                                <td class="border border-gray-300 p-2">Dr. Ade Rusman, S.Pt., M.M., M. Si.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Dr. apt. Agus S, S.Si., M.Si.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold" rowspan="3">Pelayanan dan Sertifikasi Halal</td>
+                                <td class="border border-gray-300 p-2">Ir. Regawa Bayu P, M.T.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Arif Prashadi S, TP., M.Sc.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2">Abdul Haris Mulyadi, S.T., M.T.</td>
+                            </tr>
+                            <tr>
+                                <td class="border border-gray-300 p-2 font-semibold">Lembaga Pendampingan Proses Produk Halal</td>
+                                <td class="border border-gray-300 p-2">Dr. apt. Diniatik, M.Sc.</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
