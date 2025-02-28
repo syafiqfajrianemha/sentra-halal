eb2dbdeb3df9435c15ab48271ccb5838802b9bb5
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-28 10:22:08 +0700

    `add` : view contact

--- resources/views/user/contact/index.blade.php
+++ resources/views/user/contact/index.blade.php
@@ -1,15 +1,21 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Kontak") }}
+            <div class="flex flex-col md:flex-row items-center md:items-stretch gap-6">
+                <div class="w-full md:w-1/2 flex justify-center items-center">
+                    <div class="w-full h-[300px] md:h-[400px] bg-gray-200">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3956.4615913373373!2d109.2725249!3d-7.414055599999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e655f2f36eb22d1%3A0x9a30de3e264d87d4!2sHalal%20Center%20UMP!5e0!3m2!1sid!2sid!4v1740711936648!5m2!1sid!2sid"
+                            class="w-full h-full border-0"
+                            allowfullscreen="" loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade">
+                        </iframe>
+                    </div>
+                </div>
+
+                <div class="w-full md:w-1/2 flex flex-col justify-center gap-4">
+                    <button class="bg-blue-500 px-4 py-3 rounded-lg text-center text-white shadow">WhatsApp</button>
+                    <button class="bg-blue-500 px-4 py-3 rounded-lg text-center text-white shadow">Email</button>
                 </div>
             </div>
         </div>
