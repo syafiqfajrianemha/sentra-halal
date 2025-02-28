4e6a13af461f7ef2dfed1efef0ab78c5d09e626b
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-28 09:40:05 +0700

    `add` : view gallery

--- resources/views/user/gallery/index.blade.php
+++ resources/views/user/gallery/index.blade.php
@@ -1,15 +1,30 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Galeri") }}
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+                <div class="bg-white shadow rounded-lg p-4 flex flex-col items-center">
+                    <div class="flex items-center justify-center">
+                        <img src="{{ asset('images/galeri1.jpg') }}" alt="Logo">
+                    </div>
+                    <div class="mt-2 w-full py-2 text-center rounded">
+                        Sertifikasi Halal Industri Makanan – Tahu Bakso Neng Esty – Cilacap
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg p-4 flex flex-col items-center">
+                    <div class="flex items-center justify-center">
+                        <img src="{{ asset('images/galeri2.jpg') }}" alt="Logo">
+                    </div>
+                    <div class="mt-2 w-full py-2 text-center rounded">
+                        Sertifikasi Halal Industri Makanan – Gudeg Pincuk Malioboro - Yogyakarta
+                    </div>
+                </div>
+                <div class="bg-white shadow rounded-lg p-4 flex flex-col items-center">
+                    <div class="flex items-center justify-center">
+                        <img src="{{ asset('images/galeri3.png') }}" alt="Logo">
+                    </div>
+                    <div class="mt-2 w-full py-2 text-center rounded">
+                        Lembaga Pelatihan - Kerja Sama dengan LSP UMP
+                    </div>
                 </div>
             </div>
         </div>
