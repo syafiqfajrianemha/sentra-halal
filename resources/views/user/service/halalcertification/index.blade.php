d1f01889b6ced4da47c21de2f2cbbc50b02ccffa
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-28 10:10:55 +0700

    `add` : view service

--- resources/views/user/service/halalcertification/index.blade.php
+++ resources/views/user/service/halalcertification/index.blade.php
@@ -1,15 +1,18 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Pengajuan Sertifikasi Halal") }}
+            <div class="flex flex-col md:flex-row items-center md:items-start gap-6">
+                <div class="flex flex-col gap-4 w-full md:w-2/4">
+                    <button class="bg-blue-500 px-4 py-3 rounded-lg text-center text-white shadow">Panduan</button>
+                    <button class="bg-blue-500 px-4 py-3 rounded-lg text-center text-white shadow">Syarat Pengajuan</button>
+                    <button class="bg-blue-500 px-4 py-3 rounded-lg text-center text-white shadow">Pengajuan Sertifikat Halal</button>
+                    <button class="bg-blue-500 px-4 py-3 rounded-lg text-center text-white shadow">Unduh Sertifikat</button>
+                </div>
+
+                <div class="w-full md:w-3/4 flex justify-center">
+                    <div class="w-100 h-100 flex items-center justify-center">
+                        <img src="{{ asset('images/sertif.png') }}" alt="Logo">
+                    </div>
                 </div>
             </div>
         </div>
