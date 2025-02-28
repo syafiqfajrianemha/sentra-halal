d0b736ac2210d3b2667c5f0f8dd249569161a08f
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-28 09:31:48 +0700

    `add` : view activity

--- resources/views/user/activity/index.blade.php
+++ resources/views/user/activity/index.blade.php
@@ -1,15 +1,104 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Kegiatan") }}
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/bimbingan.jpg') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">BIMBINGAN TEKNIS SISTEM JAMINAN PRODUK HALAL</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Bimbingan Teknis Sistem Jaminan Produk Halal adalah sebuah bimbingan mengenai pembuatan suatu sistem jaminan produk halal yang terintegrasi disusun, diterapkan, dan dipelihara untuk mengatur Bahan, proses produksi, Produk, sumber daya, dan prosedur dalam rangka menjaga kesinambungan PPH.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/sosialisasi1.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">SOSIALISASI</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Sosialisasi dan Pembagian Sertifikat Halal di Desa Pekunden, Banyumas.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/sosialisasi2.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">SOSIALISASI</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Sosialisasi Sertifikat Halal di Desa Binangun, Banyumas.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/kerjasama1.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">KERJASAMA</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            MoU dengan Chulalongkorn University.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/kerjasama2.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">KERJASAMA</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Bimbingan Teknis Sisitem Jaminan Produk Halal - Kerja Sama Bank Indonesia.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/kerjasama3.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">KERJASAMA</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Pembentukan Zona KHAS (Kuliner Halal Aman Sehat) - Kerja Sama Bank Indonesia.
+                        </p>
+                    </div>
+                </div>
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-start mb-6 md:mb-0">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/kerjasama4.png') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
+
+                    <div class="w-full md:w-1/2">
+                        <h2 class="text-lg font-bold mb-4">KERJASAMA</h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Workshop RT-PCR - Kerja Sama PT Andaru Analitika Sains.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
