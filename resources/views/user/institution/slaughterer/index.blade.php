bad6f1857725337c0a039ac4fe2c476c65400f71
Syafiq Fajrian Emha <syafiqfajrianemha@gmail.com>
2025-02-28 09:11:33 +0700

    `add` : view institution

--- resources/views/user/institution/slaughterer/index.blade.php
+++ resources/views/user/institution/slaughterer/index.blade.php
@@ -1,15 +1,22 @@
 <x-guest-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("Halaman Juru Sembelih Halal") }}
+                <div class="p-6 flex flex-col md:flex-row items-center justify-between">
+                    <div class="w-full md:w-1/2 mb-6 md:mb-0">
+                        <h2 class="text-lg font-bold mb-4">JURU SEMBELIH HALAL </h2>
+                        <p class="text-sm text-gray-700 mb-4 text-justify">
+                            Juru Sembelih Halal (JULEHA) adalah sebuah profesi terkait proses penyembelihan hewan ternak sesuai syariat islam.
+                        </p>
+                        <h3 class="text-md font-semibold mt-4">Target Peserta  : Juleha RPH-U, Takmir Masjid, Rumah Aqiqah, dan Umum</h3>
+                        <h3 class="text-md font-semibold mt-4">Total Juleha Terdaftar di Sentra Halal : 319 orang</h3>
+                    </div>
+
+                    <div class="w-full md:w-1/2 flex justify-center md:justify-end">
+                        <div class="w-100 h-100 md:w-100 md:h-100 bg-white flex items-center justify-center">
+                            <img src="{{ asset('images/juleha.jpg') }}" alt="Logo" class="max-w-full h-auto">
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
